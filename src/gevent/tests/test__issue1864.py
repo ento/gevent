47fa5f25294b57e0bb71885d5324e2b50b7682b6
ento <ento+github@i.pearlwaffles.xyz>
2022-09-09 23:23:14 -0800

    Prefer regression testing over cautionary comment

---
+++ src/gevent/tests/test__issue1864.py
@@ -0,0 +1,17 @@
+import math
+import unittest
+
+from gevent.testing import PY39
+
+
+class TestSubnormalFloatsAreNotDisabled(unittest.TestCase):
+    """
+    Enabling the -Ofast compiler flag resulted in subnormal floats getting
+    disabled the moment when gevent was imported. This impacted libraries
+    that expect subnormal floats to be enabled.
+    """
+    @unittest.skipUnless(PY39, "Need math.nextafter()")
+    def test_subnormal_is_not_zero(self):
+        import gevent
+
+        assert math.nextafter(0, 1) != 0
